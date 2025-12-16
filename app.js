2d2ced49fd232199577176cde5c87d4f8d44e1fd
siryansh <siryanshtyagi@gmail.com>
2025-12-16 23:45:15 +0530

    Updated app.js for some fix or feature

--- app.js
+++ app.js
@@ -265,7 +265,7 @@ app.use((req, res, next) => {
 
 // root route
 app.get("/", (req, res) => {
-  res.send("root route...");
+  res.redirect("/listings");
 });
 
 // Demo user route (optional - for testing)
